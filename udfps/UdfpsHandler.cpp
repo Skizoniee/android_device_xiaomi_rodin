557954de2661f9c467058c727b3df1a4be9639d3
Ido <dev.xyzuniverse@gmail.com>
2025-10-25 17:58:34 +0200

    rodin: udfps: Add xiaomi-touch mechanism to udfpshandler - The xiaomi-touch way on rodin is differ than general xiaomi devices has. - Taken from reversed-engineering AIDL touchfeature service
    Change-Id: Ic699613a38ab6b7279a4c7b6d3791426485acfbf
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -14,6 +14,7 @@
 #include <sys/ioctl.h>
 #include <fstream>
 #include <thread>
+#include <cstring>
 
 #include "UdfpsHandler.h"
 #include "mi_disp.h"
@@ -36,9 +37,83 @@
 
 #define FINGERPRINT_ACQUIRED_VENDOR 7
 
+// Touch device constants
+#define TOUCH_DEV_PATH "/dev/xiaomi-touch"
+#define TOUCH_ID 0
+#define IOCTL_SET_TOUCH_ID 0x40005403
+#define IOCTL_TOUCH_OPERATION 0xc4085400
+
+// Touch modes
+#define Touch_Fod_Enable 10
+
+// Touch command types
+#define CMD_SET_CUR_VALUE 0
+
 using ::aidl::android::hardware::biometrics::fingerprint::AcquiredInfo;
 
 namespace {
+
+// Touch ioctl data structure
+struct CommonDataPacket {
+    uint8_t touchId;
+    uint8_t command;
+    uint16_t mode;
+    uint16_t length;
+    uint16_t reserved;
+    int buffer[256];
+
+    CommonDataPacket() : touchId(0), command(0), mode(0),
+                         length(0), reserved(0) {
+        memset(buffer, 0, sizeof(buffer));
+    }
+};
+
+// Global touch device file descriptor
+static int touchDeviceFd = -1;
+
+/**
+ * Initialize touch device for FOD operations
+ */
+static bool initTouchDevice() {
+    if (touchDeviceFd >= 0) {
+        return true;  // Already initialized
+    }
+
+    touchDeviceFd = open(TOUCH_DEV_PATH, O_RDWR);
+    if (touchDeviceFd < 0) {
+        LOG(ERROR) << "Failed to open touch device: " << strerror(errno);
+        return false;
+    }
+
+    // Set touch device ID
+    int result = ioctl(touchDeviceFd, IOCTL_SET_TOUCH_ID, (unsigned long)TOUCH_ID);
+    if (result < 0) {
+        LOG(ERROR) << "Failed to set touch ID: " << strerror(errno);
+        close(touchDeviceFd);
+        touchDeviceFd = -1;
+        return false;
+    }
+
+    LOG(DEBUG) << "Touch device initialized successfully";
+    return true;
+}
+
+/**
+ * Set touch FOD mode using xiaomi_touch_ioctl
+ */
+static void setTouchFodMode(int value) {
+    if (!initTouchDevice()) {
+        LOG(ERROR) << "Touch device not initialized, cannot set FOD mode";
+        return;
+    }
+    CommonDataPacket packet;
+    packet.touchId = TOUCH_ID;
+    packet.command = CMD_SET_CUR_VALUE;
+    packet.mode = Touch_Fod_Enable;
+    packet.length = 1;
+    packet.buffer[0] = value;
+    ioctl(touchDeviceFd, IOCTL_TOUCH_OPERATION, &packet);
+}
 
 template <typename T>
 static void set(const std::string& path, const T& value) {
@@ -90,6 +165,9 @@ class XiaomiRodinUdfpsHandler : public UdfpsHandler {
         mDevice = device;
         disp_fd_ = android::base::unique_fd(open(DISP_FEATURE_PATH, O_RDWR));
 
+        // Initialize touch device
+        initTouchDevice();
+
         // Thread to listen for fod ui changes
         std::thread([this]() {
             int fd = open(DISP_FEATURE_PATH, O_RDWR);
@@ -140,6 +218,7 @@ class XiaomiRodinUdfpsHandler : public UdfpsHandler {
     }
 
     void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
+        setTouchFodMode(1);
         mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_STATUS, PARAM_FOD_PRESSED);
         // Request HBM
         disp_local_hbm_req req;
@@ -150,8 +229,8 @@ class XiaomiRodinUdfpsHandler : public UdfpsHandler {
     }
 
     void onFingerUp() {
+        setTouchFodMode(0);
         mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_STATUS, PARAM_FOD_RELEASED);
-        // Request to disable HBM
         disp_local_hbm_req req;
         req.base.flag = 0;
         req.base.disp_id = MI_DISP_PRIMARY;
@@ -197,6 +276,7 @@ class XiaomiRodinUdfpsHandler : public UdfpsHandler {
     android::base::unique_fd disp_fd_;
 
     void setFodStatus(int value) {
+        setTouchFodMode(value);
         set(FOD_STATUS_PATH, value);
     }
 };
