368c0b39c6599e3f7e233cc8263815ffd67f1ae0
Zeydann <zeydann.sc@gmail.com>
2026-05-25 09:58:29 +0700

    rodin: bring up LineageOS
    Signed-off-by: Zeydann <zeydann.sc@gmail.com>

--- lineage_rodin.mk
+++ lineage_rodin.mk
@@ -8,17 +8,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lumine stuff.
-$(call inherit-product, vendor/lumine/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from rodin device
 $(call inherit-product, device/xiaomi/rodin/device.mk)
 
-# Flags
-LUMINE_MAINTAINER := XZ
-
 PRODUCT_DEVICE := rodin
-PRODUCT_NAME := lumine_rodin
+PRODUCT_NAME := lineage_rodin
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 2412DPC0AG
 PRODUCT_MANUFACTURER := xiaomi
