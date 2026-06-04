e56e610167aa9b392ac504548019243f471df5c4
Ido <dev.xyzuniverse@gmail.com>
2026-06-04 12:35:58 +0200

    rodin: Ship MiuiCamera
    Change-Id: I64bef44871ebc76efa1b0d9fbef713025c06e9f3
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>
    Signed-off-by: Zeyy <zeydann.sc@gmail.com>

--- lineage_rodin.mk
+++ lineage_rodin.mk
@@ -13,6 +13,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from rodin device
 $(call inherit-product, device/xiaomi/rodin/device.mk)
+TARGET_SHIPS_MIUICAMERA := true
 
 PRODUCT_DEVICE := rodin
 PRODUCT_NAME := lineage_rodin
@@ -26,7 +27,7 @@ PRODUCT_SYSTEM_DEVICE := rodin
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="missi-user 15 AP3A.240905.015.A2 OS2.0.204.0.VOJMIXM release-keys" \
-    BuildFingerprint=POCO/rodin_global/rodin:15/AP3A.240905.015.A2/OS2.0.204.0.VOJMIXM:user/release-keys \
+    BuildDesc="missi-user 15 AP3A.240905.015.A2 OS3.0.10.0.WOJMIXM release-keys" \
+    BuildFingerprint=POCO/rodin_global/rodin:15/AP3A.240905.015.A2/OS3.0.10.0.WOJMIXM:user/release-keys \
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
