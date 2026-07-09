ee23f342c5bcf75743f2fed4a310d1b983728a45
Zeyy <zeydann.sc@gmail.com>
2026-07-09 17:18:23 +0700

    rodin: setup: sync CC setup with latest change
    Signed-off-by: Zeyy <zeydann.sc@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -39,9 +39,8 @@ if [ ! -f "$SETUP_MARKER" ]; then
         git fetch https://github.com/Zeydann/frameworks_base-cc.git 16.2
         git cherry-pick a1560f533aa7a4fe2aa7af414832a3b2ea3020ea..3bb10da60f3b135b73618a0b3523746bf774de21
         git cherry-pick \
-            44c78c544555541ad766aab4e693869b0bc43240 \
-            117f0d956631d79afe18f83b813d71aea91cc213 \
-            cddcc2bd5abedd2308dd7f4ec8878491e4b4d3b6
+         e37bd033fb3417ce0931f118cccdef191f67ccb8 \
+         434b4952c955337efee64a5fe3a782cee11defe4
 
         cd ../..
     fi
@@ -52,9 +51,13 @@ if [ ! -f "$SETUP_MARKER" ]; then
 
         git fetch https://github.com/Zeydann/packages_apps_Settings-cc.git 16.2
         git cherry-pick \
-            2221b81f07042f17b03503b66f2ddf5dc5ebf4cf \
-            6d57e606653065bc76b6d55251f8e2131b3773ca \
-            7a8639682ee29ef4c25d6fa06a3c21a1bc2b22e5
+         2221b81f07042f17b03503b66f2ddf5dc5ebf4cf \
+         6d57e606653065bc76b6d55251f8e2131b3773ca \
+         7a8639682ee29ef4c25d6fa06a3c21a1bc2b22e5 \
+         cd73c90913c4aaf9a24af0c88809de55a90fbd25 \
+         d7302a121a0210fd1eacb2a460046737dde9b57c \
+         9bb703d9495c9c1603e149fc119a1050fb85d772 \
+         6afe3e1381cf16d88be16eda21e82477fe7b6749
 
         cd ../../..
     fi
